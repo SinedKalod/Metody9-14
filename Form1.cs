d54bce598f51144c92eb057c82182b10389006fa
dolakd.04 <dolakd.04@spst.eu>
2022-03-07 13:00:57 +0100

    dvemetody

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody9_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = double.Parse(textBox3.Text);
+            double x1, x2, d;
+            Metody.Diskriminant(a, b, c, out x1, out x2, out d);
+            if (d < 0) MessageBox.Show("Rovnice nema reseni v R");
+            else if (d > 0) MessageBox.Show("Koren x1 je: " + x1 + " a koren x2 je: " + x2);
+            else MessageBox.Show("Rovnice ma jedno dvojnasobne reseni " + x1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string idk = textBox4.Text;
+            int cifSoucet = 0, lichCifSoucet = 0, sudCifSoucet = 0;
+            if (Metody.ObsahujeCislici(idk, out cifSoucet, out lichCifSoucet, out sudCifSoucet))
+            {
+                MessageBox.Show("Obsahuje číslici");
+                MessageBox.Show("Ciferný součet je " + cifSoucet);
+                MessageBox.Show("Ciferný součet lichých čísel je " + lichCifSoucet);
+                MessageBox.Show("Ciferný součet sudých čísel je " + sudCifSoucet);
+            }
+            else MessageBox.Show("Neobsahuje cifry");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Metody.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody9_14
+{
+        class Metody
+        {
+            public static double Diskriminant(double a, double b, double c, out double x1, out double x2, out double d)
+            {
+                d = (b * b) - (4 * a * c);
+                x1 = 0;
+                x2 = 0;
+                if (d < 0)
+                {
+                    d = 0;
+                }
+                else if (d > 0)
+                {
+                    x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                }
+                else
+                {
+                    x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                }
+                return d;
+            }
+            public static bool ObsahujeCislici(string idk, out int cifSoucet, out int lichCifSoucet, out int sudCifSoucet)
+            {
+                bool obsahujeCislici = false;
+                cifSoucet = 0;
+                lichCifSoucet = 0;
+                sudCifSoucet = 0;
+                for (int i = 0; i < idk.Length; ++i)
+                {
+                    if (Char.IsNumber(idk[i]))
+                    {
+                        obsahujeCislici = true;
+                        cifSoucet += int.Parse(idk[i].ToString());
+
+                        if (int.Parse(idk[i].ToString()) % 2 != 0)
+                        {
+                            lichCifSoucet += int.Parse(idk[i].ToString());
+                        }
+                        else if (int.Parse(idk[i].ToString()) % 2 == 0)
+                        {
+                            sudCifSoucet += int.Parse(idk[i].ToString());
+                        }
+                    }
+                }
+
+                return obsahujeCislici;
+            }
+            public static int PocetSlov(string s, out string upravS)
+            {
+                int PocetSlov = 0;
+                string[] oddeleno;
+                char[] separator = { ' ' };
+                oddeleno = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                PocetSlov = oddeleno.Length;
+                int i = 0;
+                while (i < s.Length)
+                {
+                    if (s[i] >= '0' && s[i] <= '9')
+                    {
+                        upravS = s.Remove(i, 1);
+                    }
+                    else ++i;
+                }
+                upravS = s;
+                return PocetSlov;
+            }
+        }
+    }
+
